dee8eb898fa49562f9cf3dcc605dcbfe5f2dad59
IvanK <ivan.kovalov@spsoft.com>
2023-04-06 18:27:27 +0300

    add api test

--- pages/Api.ts
+++ pages/Api.ts
@@ -1,10 +1,13 @@
 import { APIRequestContext } from "@playwright/test";
+import { LoginApiPage } from "./api_login_page";
 
 export class Api {
     apiContext: any
+    loginPage: LoginApiPage;
 
     constructor(apiContext: APIRequestContext) {
         this.apiContext = apiContext
+        this.loginPage = new LoginApiPage(apiContext)
 
     }
     
