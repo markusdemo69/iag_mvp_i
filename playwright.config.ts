c96ab26a6d6521bb7ad33b447ec14a5f63d8ba38
IvanK <ivan.kovalov@spsoft.com>
2023-04-06 22:00:54 +0300

    upd

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = {
     ['allure-playwright']
   ],
   use: {
-    headless: true,
+    headless: false,
     viewport: { width: 1960, height: 1280 },
     ignoreHTTPSErrors: true,
     video: 'on-first-retry',
